3627bf17ba8baa1a08ae9f82c3381952a31cfe65
alchavez-9 <390610@student.linnmar.org>
2026-01-13 10:05:10 -0600

    Tuning (help)
    switched main measurement for tuning to MM (somehow that is working better now)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -29,24 +29,25 @@ public class Constants {
         .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
         .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
         .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD);
+        //.xVelocity(217.5953); (other test got 156) (183.2711)
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
     public static PinpointConstants localizerConstants = new PinpointConstants()
         .forwardPodY(-0.89217913386) // in inches
         .strafePodX(-3.104086614)    // please dont kill me thanks
-            .distanceUnit(DistanceUnit.INCH)
+            .distanceUnit(DistanceUnit.MM)
             .hardwareMapName("pinpoint")
-            .customEncoderResolution(8192/(Math.PI * 1.3662))
-            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED)//; //finally works
+            .customEncoderResolution(2048/(Math.PI * 35)) //trying this with quad resolution
+            //.forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED)//; //finally works
             .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED);
 
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
-            .pathConstraints(pathConstraints)
-            .mecanumDrivetrain(driveConstants)
-            .pinpointLocalizer(localizerConstants)
-            .build();
+                .pinpointLocalizer(localizerConstants)
+                .pathConstraints(pathConstraints)
+                .mecanumDrivetrain(driveConstants)
+                .build();
     }
 }
